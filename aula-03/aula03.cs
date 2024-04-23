d742bc7c31949a89ae57d81bb5ffa07de23063a2
NerdSemProerd <emanuellrossetto@hotmail.com>
2024-04-23 09:38:10 -0300

    Adicionando aulas

---
+++ aula-03/aula03.cs
@@ -0,0 +1,12 @@
+using System;
+class Aula03{
+    static void Main(){
+        int num1, num2, res;
+
+        num1=10;
+        num2=2;
+        res = num1 * num2;
+
+        Console.WriteLine("Multipricação de " + num1 + " com2 " + num2 + " é igual a: " + res);
+    }
+}
